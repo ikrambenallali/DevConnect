0cfbfbb7f9ba6fe8cf38553b4231634904c235c0
ikrambenallali <ikrambenallali2003@gmail.com>
2025-02-27 21:58:38 +0100

    modifier post

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -255,7 +255,7 @@
                     </div>
                     <!-- Edit/Delete for post owner -->
                     <div class="flex space-x-3">
-                        <a href="" class="text-gray-500 hover:text-gray-700 flex items-center">
+                        <a href="{{ route('editPost',$post->id) }}" class="text-gray-500 hover:text-gray-700 flex items-center">
                             <svg class="w-5 h-5 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
                             </svg>
@@ -271,7 +271,7 @@
                                 </svg>
                                 Delete
                             </button>
-                            
+
                         </form>
                     </div>
                 </div>
