8bc4fc1906a601daec5703960b0ea1d3c08f35de
ikrambenallali <ikrambenallali2003@gmail.com>
2025-02-28 18:25:21 +0100

    add likes

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,6 +14,9 @@ class Post extends Model
     public function comments(){
         return $this->hasMany(Commantaire::class);
     }
+    public function likes(){
+        return $this->hasMany(Like::class);
+    }
     public function user(){
         return $this->belongsTo(User::class);
     }
@@ -21,4 +24,9 @@ class Post extends Model
     {
         return $this->belongsToMany(Tag::class, 'post_tag');
     }
+    public function isLikedBy($user)
+    {
+        return $this->likes()->where('user_id', $user->id)->exists();
+    }
+
 }
